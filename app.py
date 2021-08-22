b5a36320f9d79c76b28094c60480ce128d6a8d4c
yulong <1402548603@qq.com>
2021-08-22 22:31:17 +0800

    add

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
-from flask import Flask
-from jinja2 import Markup
+from flask import Flask, render_template
 
-from pyecharts import options as opts
-from pyecharts.charts import Bar
-
-app = Flask(__name__, static_folder="templates")
+app = Flask(__name__, template_folder="templates")
 
 
-def bar_base() -> Bar:
-    c = (
-        Bar()
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
-            .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
-            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
-    )
-    return c
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    c = bar_base()
-    return Markup(c.render_embed())
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
